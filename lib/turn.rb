5ad4d90a23fa7cbac3074af3d309d0b0572588d5
Harrison Hascoe <hascoeharrison@gmail.com>
2020-08-30 22:26:57 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,6 +6,12 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(user_input)
+  index = user_input.to_i
+  index -= 1
+  return index
+end
+
 def valid_move?
   
 
